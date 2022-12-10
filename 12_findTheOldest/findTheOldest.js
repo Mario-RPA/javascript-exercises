e7523e3714a23d250a5a6a419c95bff6f428d25f
Mario Orquera <mario.orquera.p@gmail.com>
2022-12-10 14:34:06 -0300

    Add working solution to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
 const findTheOldest = function(person) {
-    person.reduce(
+    return person.reduce(
         (oldest, currentPerson) => {
             if(!currentPerson.hasOwnProperty("yearOfDeath")) {
                 currentPerson.yearOfDeath = new Date().getFullYear();
@@ -12,21 +12,21 @@ const findTheOldest = function(person) {
 
 };
 
-let oldest = findTheOldest({
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-    },
-    {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-    },
-    {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-    });
+// let oldest = findTheOldest([{
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//     },
+//     {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//     },
+//     {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//     }]);
 
 // // Do not edit below this line
 module.exports = findTheOldest;
